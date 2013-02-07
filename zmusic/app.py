e11cb660b253dcb463e35adfe136b931d18ac605
Michael Cooper <mic159@gmail.com>
2013-02-07 21:54:31 +1100

    Entry point for uwsgi

--- zmusic/app.py
+++ zmusic/app.py
@@ -5,12 +5,15 @@ from zmusic.database import db
 from zmusic.endpoints import load_endpoints
 from zmusic.login import login_manager
 
-def main():
+def create_app():
 	app = Flask(__name__)
 	app.config.from_pyfile(resource_filename('zmusic', 'app.cfg'))
 	db.init_app(app)
 	db.app = app
 	login_manager.setup_app(app)
-	
 	load_endpoints(app)
+	return app
+
+def main():
+	app = create_app()
 	app.run()
